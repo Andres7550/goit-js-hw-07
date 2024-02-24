94fa7501b00b834258c213cda7031eb17a00029d
Andres7550 <119809226+Andres7550@users.noreply.github.com>
2024-02-23 23:14:51 -0500

    Avance tarea 7 js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,27 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector('.gallery');
-
-galleryItems.forEach(item => {
-    const galleryItem = document.createElement('li');
-    galleryItem.classList.add('gallery__item');
-
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.href = item.original;
-
-    const image = document.createElement('img');
-    image.classList.add('gallery__image');
-    image.src = item.original;
-    image.alt = item.description;
-
-    link.appendChild(image);
-    galleryItem.appendChild(link);
-    gallery.appendChild(galleryItem);
+console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+function createGallery(galleryItems) {
+    let htmlString = "";
+    galleryItems.forEach(({ preview, original, description }) => {
+        htmlString += `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+            </a>
+        </li>`
+    });
+    return htmlString;
+}
+gallery.insertAdjacentHTML("afterbegin", createGallery(galleryItems));
+gallery.addEventListener("click", openModal);
+function openModal(e) {
+    e.preventDefault();
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: "250ms",
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
-});
-
 console.log(galleryItems);
